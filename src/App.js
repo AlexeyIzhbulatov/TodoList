dbc13382bf26b95ea10335ccc479414178faada0
AlexeyIzhbulatov <ouren@mail.ru>
2020-07-14 07:22:04 +0500

    TodoList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
 import TodoList from "./TodoList";
+import CreateForm from "./CreateForm";
+
 
 const initialTodos = [
   {id: uuidv4(), name: 'Test1', done: false},
@@ -12,9 +14,14 @@ function App() {
 
   const [todos, setTodos] = useState(initialTodos);
 
+  const onChangeTask = (task) => {
+    console.log(task)
+  }
+
   return (
     <div className="App">
 <TodoList todos={todos}/>
+<CreateForm onChangeTask={onChangeTask}/>
     </div>
   );
 }
--- src/CreateForm.js
+++ src/CreateForm.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-function CreateForm() {
+function CreateForm(props) {
+  const [task, setTask] = useState('')
+
+  const onChangeTask = () => {
+    console.log(task)
+    props.onChangeTask(task)
+    setTask('')
+  }
+
   return (
     <div>
       <form>
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="text" className="form-control"/>
-        <button type="submit" className="btn btn-primary">Submit</button>
+          <label htmlFor="exampleInputEmail1">Task</label>
+          <input type="text" className="form-control" value={task} onChange={onChangeTask}/>
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="button" className="btn btn-secondary">Cancel</button>
         </div>
       </form>
     </div>
